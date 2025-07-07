98e8ea21dae6feaae851cc093c0e7d749978986f
MeghaMohandas17 <meghasalu2002@gmail.com>
2025-07-07 07:17:55 -0300

    Add files via upload

---
+++ matrixinverse.py
@@ -0,0 +1,13 @@
+import numpy as np
+a=int(input("Enter the number of rows"))
+b=int(input("Enter the number of columns:"))
+matrix=[]
+print("Enter the elements:")
+for i in range(a):
+ a=[]
+ for j in range(b):
+   a.append(int(input()))
+ matrix.append(a)
+print(matrix)
+result=np.linalg.inv(matrix)
+print(result)
